755c9d927b65e96b5e3c2da0290cecbe4ee93936
Odai Al-Obeidat <odai.al.obeidat950@gmail.com>
2023-10-13 01:23:03 +0300

    odai

---
+++ Hospital-System/Hospital-System/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Hospital_System.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital_System.Controllers
+{
+	public class HomeController : Controller
+	{
+		// Home Page
+		private readonly HospitalDbContext _cotext;
+
+		public HomeController(HospitalDbContext cotext)
+		{
+			_cotext = cotext;
+		}
+		public IActionResult Index()
+		{
+
+			return View();
+		}
+
+	}
+}
