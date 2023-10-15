9a2a55a4385e8acade8cdb37209a1398576548e7
marwasyr <marwaalahdab14@gmail.com>
2023-10-15 19:34:35 +0300

    selectDep style

--- Hospital-System/Hospital-System/Controllers/AppointmentsController.cs
+++ Hospital-System/Hospital-System/Controllers/AppointmentsController.cs
@@ -118,42 +118,6 @@ namespace Hospital_System.Controllers
 
 
 
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
-
-
-        //[HttpPost]
-        //public async Task<IActionResult> Create(AppointmentDTO appointmentDTO)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var createdAppointment = await _appointmentService.CreateAppointment(appointmentDTO);
-        //        if (createdAppointment != null)
-        //        {
-        //            return RedirectToAction(nameof(Index));
-        //        }
-        //    }
-
-        //    // If validation fails or time slot is not available, return to the create view with an error message
-        //    ModelState.AddModelError(string.Empty, "Appointment creation failed. Please check the selected date and time.");
-        //    return View(appointmentDTO);
-        //}
-
-
-
-
-
-        //public IActionResult SelectTime(int doctorId)
-        //{
-        //    // Generate or retrieve available time slots for the selected department and date.
-        //    DateTime selectedDate = DateTime.Today; // You can change this to the selected date.
-        //    var timeSlots = GenerateTimeSlots(selectedDate, departmentId);
-
-        //    // Pass the time slots to the view.
-        //    return View(timeSlots);
-        //}
 
 
     }
