bc3a90cc911527f11f5d97a8ba05112ffa337072
HadeelLafi <hadeellafi9@gmail.com>
2023-10-22 22:05:44 +0300

    testDontMerge

---
+++ Hospital-System/Hospital-System/Models/MedicineMedicalReport.cs
@@ -0,0 +1,11 @@
+﻿namespace Hospital_System.Models
+{
+    public class MedicineMedicalReport
+    {
+           public int MedicialReportID { get; set; }
+            public int MedicineID { get; set; }
+            public int MedicinePortion { get; set; }
+
+
+    }
+}
